d77714f119dbca5cd770399f41a85127c4cafb73
Clickers331 <61944296+Clickers331@users.noreply.github.com>
2020-04-08 17:24:59 +0300

    Enemy script

---
+++ New Unity Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Variables
+    [SerializeField] public float range = 10f;
+    [SerializeField] public float speed = 10f;
+    [SerializeField] public GameObject throwable;
+    [SerializeField] public Rigidbody2D rb;
+    [SerializeField] private RaycastHit2D hit;
+    void Start()
+    {
+        hit = Physics2D.Raycast(transform.position,transform.right);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //Functions
+       Movement();
+    }
+
+    void Movement()
+    {
+        if(hit.gameObject.CompareTag("Player"))
+        {
+            //Move towards the player if it sees him
+            Vector2.MoveTowards(transform.position, hit.gameobject.transform.position,speed*Time.deltaTime);
+        }
+    }
+}
